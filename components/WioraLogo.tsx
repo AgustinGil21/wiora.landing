792ff3c19e35481fcb75a31e895b99a63cc349a7
Agustin Gil <agustingil83@gmail.com>
2025-07-23 14:48:51 -0300

    Mobile Header

--- components/WioraLogo.tsx
+++ components/WioraLogo.tsx
@@ -2,9 +2,9 @@ import { WioraIcon } from '@/icons/WioraIcon';
 
 const WioraLogo = () => {
   return (
-    <div aria-label='logo' className='flex gap-2 items-end justify-center'>
+    <div aria-label='logo' className='flex gap-2 items-center justify-center'>
       <WioraIcon className='w-14 h-14' />
-      <span className='text-3xl font-bold mb-1.5'>Wiora</span>
+      <span className='text-2xl lg:text-3xl font-bold'>Wiora</span>
     </div>
   );
 };
